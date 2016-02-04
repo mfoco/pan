10da31bd8b1127fb272968460fc388ee5d7e4331
Marco Foco <pan@spinningkids.org>
2016-02-04 10:02:55 +0100

    splitted integer-assignment constructor (fitting/nonfitting) Fixed tests

--- pan/integer.h
+++ pan/integer.h
@@ -7,81 +7,80 @@
 
 namespace pan
 {
-	template <typename T> constexpr void is_constexpr_impl(T&& x) throw(...) { }
-	#define IS_CONSTEXPR(EXPR) (noexcept(::pan::is_constexpr_impl(EXPR)))
+    template <bool SIGNED, int BITS, typename... > struct select_integer_from_list;
 
+    template <bool SIGNED, int BITS, typename T, typename... Ts> struct select_integer_from_list<SIGNED, BITS, T, Ts...> : std::conditional_t<((SIGNED == std::numeric_limits<T>::is_signed) && (BITS <= std::numeric_limits<T>::digits)),
+        tag<T>,
+        select_integer_from_list<SIGNED, BITS, Ts... >>
+    { };
 
-	template <bool SIGNED, int BITS, typename... > struct select_integer_from_list;
+    template <bool SIGNED, int BITS>
+    using select_integer_t = typename select_integer_from_list<SIGNED, BITS, int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t>::type;
 
-	template <bool SIGNED, int BITS, typename T, typename... Ts> struct select_integer_from_list<SIGNED, BITS, T, Ts...> : std::conditional_t<((SIGNED == std::numeric_limits<T>::is_signed) && (BITS <= std::numeric_limits<T>::digits)),
-		tag<T>,
-		select_integer_from_list<SIGNED, BITS, Ts... >>
-	{ };
+    template <bool SIGNED, int BITS>
+    struct integer
+    {
+        using this_type = integer<SIGNED, BITS>;
+        using underlying_type = select_integer_t<SIGNED, BITS>;
+        static constexpr const int bits = BITS;
+        static constexpr const bool sign = SIGNED;
+        static constexpr const int digits = bits + sign;
+    private:
+        underlying_type _value;
+    public:
+        static constexpr const underlying_type min_value = sign ? (~(static_cast<underlying_type>(1) << BITS))+1 : 0; // TODO: fix warnings
+        static constexpr const underlying_type max_value = (static_cast<underlying_type>(1) << BITS) - 1; // TODO: fix warnings
 
-	template <bool SIGNED, int BITS>
-	using select_integer_t = typename select_integer_from_list<SIGNED, BITS, int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t>::type;
+        // default constructor
+        constexpr integer() noexcept : _value() {}
 
-	template <bool SIGNED, int BITS>
-	struct integer
-	{
-		using this_type = integer<SIGNED, BITS>;
-		using underlying_type = select_integer_t<SIGNED, BITS>;
-	private:
-		underlying_type _value;
-	public:
+        template<typename U> class has_smaller_range { enum { value = ((digits <= std::numeric_limits<U>::digits) && (sign || !std::numeric_limits<U>::is_signed)) }; };
 
-		constexpr static underlying_type min_value() noexcept {
-			return SIGNED ? (-(static_cast<underlying_type>(1) << BITS)) : 0;
-		}
+        // value-checking constexpr constructor
+        template<typename U>
+        constexpr integer(U value, std::enable_if_t < std::numeric_limits<U>::is_integer && !has_smaller_range<U>::value>* = nullptr) : _value(value)
+        {
+            if (value < min_value || value > max_value) throw "Value doesn't fit."; // UB! signed/unsigned comparison. TODO: Find a better way to check
+        }
 
-		constexpr static underlying_type max_value() noexcept {
-			return (static_cast<underlying_type>(1) << BITS) - 1;
-		}
+        // non-checking constexpr constructor
+        template<typename U>
+        constexpr integer(U value, std::enable_if_t < std::numeric_limits<U>::is_integer && has_smaller_range<U>::value>* = nullptr) noexcept : _value(value)
+        {
+        }
 
-		// default constructor
-		constexpr integer() noexcept : _value() {}
+        template<bool FROM_SIGNED, int FROM_BITS, typename = std::enable_if_t<((SIGNED == FROM_SIGNED) && (BITS <= FROM_BITS) || (SIGNED && (BITS < FROM_BITS)))>>
+        constexpr integer(const this_type &other) noexcept : _value(other.value)
+        {
+        }
 
-		// constructor
-		template<typename U> 
-		constexpr integer(U value, std::enable_if_t<std::numeric_limits<U>::is_integer && (((SIGNED == std::numeric_limits<U>::is_signed) && (BITS <= std::numeric_limits<U>::digits)) || (SIGNED && !std::numeric_limits<U>::is_signed && (BITS < std::numeric_limits<U>::digits)))>* = nullptr) noexcept : _value(value)
-		{
-		}
+        // upcast - no need for overflow checks
+        template<bool FROM_SIGNED, int FROM_BITS, typename = std::enable_if_t<((SIGNED == FROM_SIGNED) && (BITS <= FROM_BITS) || (SIGNED && (BITS < FROM_BITS)))>>
+        constexpr integer(const integer<FROM_SIGNED, FROM_BITS> &other) noexcept : _value(other._value)
+        {
+        }
 
-		// value-checking constexpr constructor
-		template<typename U>
-		constexpr integer(U value, std::enable_if_t<std::numeric_limits<U>::is_integer && !(((SIGNED == std::numeric_limits<U>::is_signed) && (BITS <= std::numeric_limits<U>::digits)) || (SIGNED && !std::numeric_limits<U>::is_signed && (BITS < std::numeric_limits<U>::digits)))>* = nullptr) noexcept : _value(value)
-		{
-            if (noexcept(constexpr auto i = value)) {
-                static_assert(!IS_CONSTEXPR(value) || (value >= min_value() && value <= max_value()), "Value doesn't fit."); // UB! signed/unsigned comparison. TODO: Find a better way to check
-            }
-		}
+        // move constructor
+        constexpr integer(this_type &&other) noexcept : _value()
+        {
+            using std::swap;
+            swap(_value, other._value);
+        }
 
-		// upcast & copy constructor
-		template<bool FROM_SIGNED, int FROM_BITS, typename = std::enable_if_t<((SIGNED == FROM_SIGNED) && (BITS <= FROM_BITS) || (SIGNED && (BITS < FROM_BITS)))>>
-		constexpr integer(const integer<FROM_SIGNED, FROM_BITS> &other) noexcept : _value(other.value)
-		{
-		}
+        template<bool FROM_SIGNED, int FROM_BITS, typename = std::enable_if_t<((SIGNED == FROM_SIGNED) && (BITS <= FROM_BITS) || (SIGNED && (BITS < FROM_BITS)))>>
+        /*constexpr*/ this_type & operator = (const integer<FROM_SIGNED, FROM_BITS> &other)
+        {
+            value = other.value;
+            return *this;
+        }
 
-		// move constructor
-		constexpr integer(this_type &&other) noexcept
-		{
-            std::swap(_value, other._value);
-		}
+        constexpr underlying_type value() const { return _value; }
+    };
 
-		template<bool FROM_SIGNED, int FROM_BITS, typename = std::enable_if_t<((SIGNED == FROM_SIGNED) && (BITS <= FROM_BITS) || (SIGNED && (BITS < FROM_BITS)))>>
-		/*constexpr*/ this_type & operator = (const integer<FROM_SIGNED, FROM_BITS> &other)
-		{
-			value = other.value;
-			return *this;
-		}
-
-		constexpr underlying_type value() const { return _value; }
-	};
-
-	template<bool SIGNED, int A_BITS, int B_BITS>
-	constexpr auto operator + (const integer<SIGNED, A_BITS> &a, const integer<SIGNED, B_BITS> &b)
-	{
-		using result_type = integer<SIGNED, std::max(A_BITS, B_BITS) + 1>;
-		return result_type{ static_cast<typename result_type::underlying_type>(a) + static_cast<typename result_type::underlying_type>(b) };
-	}
+    template<bool SIGNED, int A_BITS, int B_BITS>
+    constexpr auto operator + (const integer<SIGNED, A_BITS> &a, const integer<SIGNED, B_BITS> &b)
+    {
+        using result_type = integer<SIGNED, std::max(A_BITS, B_BITS) + 1>;
+        return result_type{ static_cast<typename result_type::underlying_type>(a) + static_cast<typename result_type::underlying_type>(b) };
+    }
 }
--- pan/integer_test.cpp
+++ pan/integer_test.cpp
@@ -6,98 +6,98 @@ using namespace pan;
 
 TEST(Integer, DefaultConstruction)
 {
-	integer<false, 10> u10{};
-	integer<true, 10> s10{};
-	integer<false, 20> u20{};
-	integer<true, 20> s20{};
-	integer<false, 32> u32{};
-	integer<true, 31> s31{};
-	integer<false, 64> u64{};
-	integer<true, 63> s63{};
+    integer<false, 10> u10{};
+    integer<true, 10> s10{};
+    integer<false, 20> u20{};
+    integer<true, 20> s20{};
+    integer<false, 32> u32{};
+    integer<true, 31> s31{};
+    integer<false, 64> u64{};
+    integer<true, 63> s63{};
 
-	ASSERT_EQ(u10.value(), 0);
-	ASSERT_EQ(s10.value(), 0);
-	ASSERT_EQ(u20.value(), 0);
-	ASSERT_EQ(s20.value(), 0);
-	ASSERT_EQ(u32.value(), 0);
-	ASSERT_EQ(s31.value(), 0);
-	ASSERT_EQ(u64.value(), 0);
-	ASSERT_EQ(s63.value(), 0);
+    ASSERT_EQ(u10.value(), 0);
+    ASSERT_EQ(s10.value(), 0);
+    ASSERT_EQ(u20.value(), 0);
+    ASSERT_EQ(s20.value(), 0);
+    ASSERT_EQ(u32.value(), 0);
+    ASSERT_EQ(s31.value(), 0);
+    ASSERT_EQ(u64.value(), 0);
+    ASSERT_EQ(s63.value(), 0);
 }
 
 TEST(Integer, Construction)
 {
-	integer<false, 10> u10_10s{ 10 };
-	integer<true, 10> s10_10s{ 10 };
-	integer<false, 10> u10_10u{ 10u };
-	integer<true, 10> s10_10u{ 10u };
-	integer<false, 1> s10_10u_fail{ 10241u }; // SHOULD NOT COMPILE!!!
+    constexpr integer<false, 10> u10_10s{ 10 };
+    constexpr integer<true, 10> s10_10s{ 10 };
+    constexpr integer<false, 10> u10_10u{ 10u };
+    constexpr integer<true, 10> s10_10u{ 10u };
+    constexpr integer<false, 1> s10_10u_fail{ 10241u }; // SHOULD NOT COMPILE!!!
 
 
-	ASSERT_EQ(u10_10s.value(), u10_10u.value());
-	ASSERT_EQ(s10_10s.value(), s10_10u.value());
+    ASSERT_EQ(u10_10s.value(), u10_10u.value());
+    ASSERT_EQ(s10_10s.value(), s10_10u.value());
 }
 
 TEST(Integer, CopyConstruction)
 {
-	integer<false, 10> u10_10s1{ 10 };
-	auto u10_10s2 = u10_10s1; // FAILS on VS2015 Upd1 (constructor is deleted)
-	ASSERT_EQ(u10_10s1.value(), u10_10s2.value());
+    integer<false, 10> u10_10s1{ 10 };
+    auto u10_10s2 = u10_10s1;
+    ASSERT_EQ(u10_10s1.value(), u10_10s2.value());
 }
 
 TEST(Integer, MoveConstruction)
 {
     integer<false, 10> u10_10s1{ 10 };
     integer<false, 10> u10_10s2(std::move(u10_10s1));
-    ASSERT_EQ(u10_10s1.value(), 10);
+    ASSERT_EQ(u10_10s2.value(), 10);
 }
 
 TEST(Integer, CopyAssignment)
 {
-	FAIL();
+    FAIL();
 }
 
 TEST(Integer, MoveAssignment)
 {
-	FAIL();
+    FAIL();
 }
 
 TEST(Integer, Literals)
 {
-	FAIL();
+    FAIL();
 }
 
 TEST(Integer, Unary)
 {
-	FAIL();
+    FAIL();
 }
 
 TEST(Integer, AutoAssignment)
 {
-	FAIL();
+    FAIL();
 }
 
 TEST(Integer, Comparison)
 {
-	FAIL();
+    FAIL();
 }
 
 TEST(Integer, Addition)
 {
-	FAIL();
+    FAIL();
 }
 
 TEST(Integer, Subtraction)
 {
-	FAIL();
+    FAIL();
 }
 
 TEST(Integer, Multiplication)
 {
-	FAIL();
+    FAIL();
 }
 
 TEST(Integer, Division)
 {
-	FAIL();
+    FAIL();
 }
