8ef35d6d13b9c25d388b04210ca4150b72ae7107
Marco Foco <mfoco@nvidia.com>
2019-08-08 18:58:34 +0200

    Changed return value from evaluate to auto, fixed (cleaned up) readme.

--- pan/include/pan/expr/expr.hpp
+++ pan/include/pan/expr/expr.hpp
@@ -34,34 +34,34 @@ namespace pan::expr
 		R r;
 	};
 
-	//template<typename T, typename V> constexpr double evaluate(T &&expr, V && variables);
+	//template<typename T, typename V> constexpr auto evaluate(T &&expr, V && variables);
 
-	template<typename V, int N> constexpr double evaluate(const var<N> &expr, V && variables)
+	template<typename V, int N> constexpr auto evaluate(const var<N> &expr, V && variables)
 	{
 		return variables[N];
 	}
 	
-	template<typename V, typename T, int N> constexpr double evaluate(const std::integral_constant<T, N> &expr, V && variables)
+	template<typename V, typename T, int N> constexpr auto evaluate(const std::integral_constant<T, N> &expr, V && variables)
 	{
 		return expr();
 	}
 	
-	template<typename V, typename L, typename R> constexpr double evaluate(const sumOp<R, L> &expr, V && variables)
+	template<typename V, typename L, typename R> constexpr auto evaluate(const sumOp<R, L> &expr, V && variables)
 	{
 		return evaluate(expr.l, variables) + evaluate(expr.r, variables);
 	}
 
-	template<typename V, typename L, typename R> constexpr double evaluate(const subOp<R, L> &expr, V && variables)
+	template<typename V, typename L, typename R> constexpr auto evaluate(const subOp<R, L> &expr, V && variables)
 	{
 		return evaluate(expr.l, variables) - evaluate(expr.r, variables);
 	}
 
-	template<typename V, typename L, typename R> constexpr double evaluate(const mulOp<R, L> &expr, V && variables)
+	template<typename V, typename L, typename R> constexpr auto evaluate(const mulOp<R, L> &expr, V && variables)
 	{
 		return evaluate(expr.l, variables) * evaluate(expr.r, variables);
 	}
 
-	template<typename V, typename L, typename R> constexpr double evaluate(const divOp<R, L> &expr, V && variables)
+	template<typename V, typename L, typename R> constexpr auto evaluate(const divOp<R, L> &expr, V && variables)
 	{
 		return evaluate(expr.l, variables) / evaluate(expr.r, variables);
 	}
